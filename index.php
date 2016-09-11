d0bdc2c507a1aa1bb9dc89679e1da0c99dcb3202
Shubham <shubham1089@gmail.com>
2016-09-11 13:38:04 +0530

    stuff

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <title>First</title>
   </head>
   <body>
-     <p>Welcome</p>
+  	<h1>Welcome</h1> 
+  	<p>The modern trend is towards the second option with no formal welcome message or statement of purpose. However, when a site is complex, it becomes essential to explain to your users, in as few simple words as possible, what they can find on your site and how they can get started.</p>
+  	<p>The London Underground is a system of electric trains which are in London, United Kingdom. It is the oldest underground railway in the world. It started running in 1863 as the Metropolitan Railway. After the opening the system was copied in many other cities, for example New York and Madrid. Even though it is called the Underground about half of it is above the ground. The "Tube" is a slang name for the London Underground, because the tunnels for some of the lines are round tubes running through the ground. The Underground has got 274 stations and over 408 km of track. From 2006 to 2007 over 1 billion passengers used the underground.</p>
+  	<h2>Image</h2> 
+  	<img src="image/mv.jpg" alt="Mountain View" style="width:800px;height:600px;">
+  	<h3>Flash</h3> 
+  	<img src="image/flash.jpg" alt="Mountain View" style="width:800px;height:600px;">
   </body>
 </html>
