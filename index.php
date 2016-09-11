9909b887479c471deb6a66c0a3a968c98a1b173d
Shubham <shubham1089@gmail.com>
2016-09-11 14:41:06 +0530

    7th

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <title>First</title>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
-  <body>
-  	<h1>Welcome</h1> 
-  	<p>The modern trend is towards the second option with no formal welcome message or statement of purpose. However, when a site is complex, it becomes essential to explain to your users, in as few simple words as possible, what they can find on your site and how they can get started.</p>
-  	<p>The London Underground is a system of electric trains which are in London, United Kingdom. It is the oldest underground railway in the world. It started running in 1863 as the Metropolitan Railway. After the opening the system was copied in many other cities, for example New York and Madrid. Even though it is called the Underground about half of it is above the ground. The "Tube" is a slang name for the London Underground, because the tunnels for some of the lines are round tubes running through the ground. The Underground has got 274 stations and over 408 km of track. From 2006 to 2007 over 1 billion passengers used the underground.</p>
-  	<h2>Image</h2> 
-  	<img src="image/mv.jpg" alt="Mountain View" style="width:200px;height:150px;">
-  	<h3>Flash</h3> 
-  	<img src="image/flash.jpg" alt="Mountain View" style="width:200px;height:100px;">
-  	<h4>GIF</h4> 
-  	<img src="image/gif.gif" alt="Mountain View" style="width:180px;height:160px;">
-  	<h5>Over</h5>
-  	<h6>Heh! Nooooooob</h6>
+  <body style="background-color:powderblue;"">
+  	<h1 id="ok" class="roll"">User Login</h1>
+  	<form>
+  		<label class="roll">   Email</label><br>
+  		<input class="roll" type="email" name="uname"><br><br>
+  		<label class="roll">Password</label><br>
+  		<input class="roll" type="password" name="pass"><br><br>
+  		<input class="roll" type="submit" value="Submit"><br><br>
+  		<input class="roll" type="reset" value="Reset">
+  	</form>
+</form> 
   </body>
 </html>
